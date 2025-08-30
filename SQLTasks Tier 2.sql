3618cc5cc8187bdcd70b70e65194ab422bbef406
BenjaminLe1 <le166@purdue.edu>
2025-08-29 20:51:26 -0500

    Part 2 start

--- SQLTasks Tier 2.sql
+++ SQLTasks Tier 2.sql
@@ -39,23 +39,33 @@ Write a SQL query to produce a list of the names of the facilities that do. */
 -- FROM Facilities
 -- WHERE membercost <> 0.0;
 
+
 /* Q2: How many facilities do not charge a fee to members? */
 
+-- SELECT Count(facid) AS HasFee
+-- FROM Facilities
+-- WHERE membercost = 0.0
+
 -- 4 facilities do not charge a fee
+
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
-SELECT facid, name, membercost, monthlymaintenance
-FROM Facilities
-WHERE membercost <> 0.0
-	AND membercost < (0.2 * monthlymaintenance);
+-- SELECT facid, name, membercost, monthlymaintenance
+-- FROM Facilities
+-- WHERE membercost <> 0.0
+-- 	AND membercost < (0.2 * monthlymaintenance);
 
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
+
+-- SELECT *
+-- FROM Facilities
+-- WHERE facid IN (1,5);
 
 
 /* Q5: Produce a list of facilities, with each labelled as
@@ -63,15 +73,35 @@ Try writing the query without using the OR operator. */
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+-- SELECT name, monthlymaintenance,
+-- 	CASE WHEN monthlymaintenance > 100
+--     		THEN 'expensive'
+--     	ELSE 
+--         	'cheap' END AS cost_label
+-- FROM Facilities;
+
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
+-- SELECT firstname, surname
+-- FROM Members
+-- WHERE joindate = (SELECT MAX(joindate)
+--                   FROM Members);
+                  
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
+-- SELECT DISTINCT CONCAT(f.name, ' - ', m.firstname, ' ', m.surname) AS court_mem
+-- FROM Bookings AS b
+-- LEFT JOIN Facilities AS f
+-- USING(facid)
+-- LEFT JOIN Members AS m
+-- USING(memid)
+-- WHERE f.name LIKE 'Tennis Court%';
 
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
@@ -81,8 +111,38 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+-- WITH fac_mem_cost AS (SELECT f.name AS fac_name, CONCAT(m.firstname, " ", m.surname) AS mem_name, 
+--                           CASE WHEN m.memid = 0 THEN f.guestcost
+--                                ELSE f.membercost END AS cost
+--                       FROM Bookings AS b
+--                       LEFT JOIN Members AS m
+--                       USING(memid)
+--                       LEFT JOIN Facilities AS f
+--                       USING(facid)
+--                       WHERE b.starttime LIKE '2012-09-14%')
+-- SELECT *
+-- FROM fac_mem_cost
+-- WHERE cost > 30
+-- ORDER BY cost DESC;
+
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
+
+-- WITH fac_mem_cost AS (SELECT f.name AS fac_name, CONCAT(m.firstname, " ", m.surname) AS mem_name, 
+--                           CASE WHEN m.memid = 0 THEN f.guestcost
+--                                ELSE f.membercost END AS cost
+--                       FROM (SELECT * FROM Bookings
+--                             WHERE starttime LIKE '2012-09-14%') AS b
+--                       LEFT JOIN Members AS m
+--                       USING(memid)
+--                       LEFT JOIN Facilities AS f
+--                       USING(facid)
+--                       )
+-- SELECT *
+-- FROM fac_mem_cost
+-- WHERE cost > 30
+-- ORDER BY cost DESC;
+
 
 
 /* PART 2: SQLite
